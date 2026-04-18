89b97bfcf41979604867b279fa7de7fd22d9365f
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2026-04-18 17:07:37 +0300

    feat: migrate to src folder

---
+++ scripts/app-icon-square-gen.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Run the script from the root of the project!
+
+# Generate square app icon, from a HQ (svg) source image.
+# Requirements: sudo apt install imagemagick
+
+########################
+# LOGO #################
+########################
+
+LOGO_SOURCE_FILE="public/icons/svg/SnapiX.logo.svg"
+LOGO_DEST_DIRECTORY="public/icons"
+
+# Generate app-icons
+for res in 192 512; do
+  convert -background transparent -resize "${res}x${res}" \
+    -background transparent -gravity center -extent "${res}x${res}" \
+    "$LOGO_SOURCE_FILE" "${LOGO_DEST_DIRECTORY}/app-icon-${res}.png"
+done
+
+for res in 192 512; do
+  convert -background transparent -resize "${res}x${res}" \
+    -background transparent -gravity center -extent "${res}x${res}" \
+    "$LOGO_SOURCE_FILE" "${LOGO_DEST_DIRECTORY}/app-icon-maskable-${res}.png"
+done
+
+for res in 36 57 60 72 76 114 120 144 152 180 192 512; do
+  convert -background transparent -resize "${res}x${res}" \
+    -background transparent -gravity center -extent "${res}x${res}" \
+    "$LOGO_SOURCE_FILE" "${LOGO_DEST_DIRECTORY}/apple-touch-icon-${res}x${res}.png"
+done
+cp "$LOGO_SOURCE_FILE" "${LOGO_DEST_DIRECTORY}/app-icon-general.svg"
+
+########################
+# FAVICON ##############
+########################
+
+FAVICON_SOURCE_FILE="public/icons/svg/SnapiX.favicon.svg"
+FAVICON_ICO_DEST_DIRECTORY="public/"
+FAVICON_SVG_DEST_DIRECTORY="public/"
+
+convert -background transparent "$FAVICON_SOURCE_FILE" -clone 0 -resize "64x64" \
+  -background transparent -gravity center -extent "64x64" \
+  "${FAVICON_ICO_DEST_DIRECTORY}/favicon.ico"
+
+cp "$FAVICON_SOURCE_FILE" "${FAVICON_SVG_DEST_DIRECTORY}/favicon.svg"
+
